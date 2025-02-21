8a0e1ab01b2d0763388fb2c2cbc07909ddcf4935
Vineet Sharma <139837499+vineetshrma7@users.noreply.github.com>
2025-02-21 11:08:06 +0530

    Update GeminiService.jsx

--- src/GeminiService.jsx
+++ src/GeminiService.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "AIzaSyChYF_ORd1yWvmW5BE_cFvSdqxPF5GEZfI"; 
+const API_KEY = "AIzaSyChYF_ORd1yWvmW5BE_cFvSdqxPF5GEZfII"; 
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 export const getGeminiResponse = async (message) => {
